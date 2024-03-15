caa1f8155e18a854a2dcdf5352d429a876566764
alewachu <ale_wachu@hotmail.com>
2024-03-14 21:04:32 -0300

    [UPDATE] Se agrega pagina de login y se agrega logout en index

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
   <FormLink />
+  <button @click="signOut()">Sign Out</button>
 </template>
+
+<script setup lang="ts">
+definePageMeta({ middleware: 'auth', auth: { guestRedirectTo: '/login' } });
+const { signOut } = useAuth();
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <button @click="signIn(`github`)">JS Sign In Github</button>
+    </div>
+    <div v-if="status === 'loading' || status === 'authenticated'">Loading...</div>
+    <div v-if="status === 'error'">Error</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { signIn, signOut, session, status, cookies, getProviders } = useAuth();
+const router = useRouter();
+if (status.value === 'authenticated') {
+  router.push('/');
+}
+</script>
