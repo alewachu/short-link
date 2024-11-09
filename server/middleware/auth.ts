c0d9fe06b722fc90fc4c6d1e9bde8d700a7c46cb
alwachu <alejandrowasilewski@gmail.com>
2024-11-09 18:55:04 -0300

    [UPDATE] Se agrega control de error

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,16 +1,20 @@
 import { userCookie } from "../utils/auth";
 
 export default defineEventHandler((event) => {
-  const path = getRequestURL(event).pathname
-  const serverSideRoutes = path.startsWith('/api');
-  const user = userCookie(event)
-  const userId = user?.id
-  if (serverSideRoutes && userId || !serverSideRoutes || path === '/api/github/callback') {
-    return
-  } else {
-    throw createError({
-      statusCode: 401,
-      statusMessage: 'Unauthorized'
-    })
+  try {
+    const path = getRequestURL(event)?.pathname
+    const serverSideRoutes = path.startsWith('/api');
+    const user = userCookie(event)
+    const userId = user?.id
+    if (serverSideRoutes && userId || !serverSideRoutes || path === '/api/github/callback') {
+      return
+    } else {
+      throw createError({
+        statusCode: 401,
+        statusMessage: 'Unauthorized'
+      })
+    }
+  } catch (error) {
+    // Error
   }
 })
